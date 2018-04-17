6612642f70b71448bcd9d829119576d4b7bcf99e
pedro <pedroleal123@hotmail.com>
2018-04-17 20:29:23 -0300

    -correção de bugs e uma pequena falha de digitação

--- src/main/java/commands/updates.java
+++ src/main/java/commands/updates.java
@@ -18,7 +18,7 @@ public class updates extends Command {
         EB.setAuthor("MUDANÇAS NO BOT");
         EB.setTitle(":bell:");
         EB.setDescription("versão 2.2.3");
-        EB.addField("mudanças:","-Conserto de bugs\n-nerf no comando ``apostar``\n  >Reduzido premio da pizza de ``3x`` para ``2x``\n  >reduzido premio da cenoura de ``2x`` para ``o valor apostado mais metade do valor apostado``\n-Adicionado o comando ``update``",false);
+        EB.addField("mudanças:","-Conserto de bugs\n-nerf no comando ``apostar``\n  >Reduzido premio da pizza de ``3x`` para ``2x``\n  >reduzido premio do chocolate de ``2x`` para ``o valor apostado mais metade do valor apostado``\n-Adicionado o comando ``update``",false);
         EB.addField("versão 2.2.4","correção de mais bugs e uma pequena falha de digitação",false);
         EB.setThumbnail(event.getSelfUser().getEffectiveAvatarUrl());
         EB.setColor(9046091);
